b37245b252f4299bcb817f1138233770001f43a7
Lucas Pearson <lpearson@trek10.com>
2022-12-01 10:50:50 -0500

    remove target

--- d1/target/debug/deps/d1p1-f0dcc057ecd3f0f7.d
+++
@@ -1,5 +0,0 @@
-/Users/lucaspearson/code/personal/github.com/catdevman/aoc2022/d1p1/target/debug/deps/d1p1-f0dcc057ecd3f0f7: src/main.rs
-
-/Users/lucaspearson/code/personal/github.com/catdevman/aoc2022/d1p1/target/debug/deps/d1p1-f0dcc057ecd3f0f7.d: src/main.rs
-
-src/main.rs:
