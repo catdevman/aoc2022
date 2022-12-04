d455b28a8fe9322e24f626576975020e86ed55e7
Lucas Pearson <catdevman@gmail.com>
2022-12-04 18:36:20 -0500

    finished d2p1

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -7,22 +7,28 @@ fn main() {
         .map(|x|{
             let line = x.split(" ").collect::<Vec<&str>>();            
             if line.len() == 2{
+                let adder = match line[1] {
+		            "X" => 1,
+		            "Y" => 2,
+                    "Z" => 3,
+                    &_ => 0
+                };
                 if (line[0] == "A" && line[1] == "X") ||
                     (line[0] == "B" && line[1] == "Y") ||
                     (line[0] == "C" && line[1] == "Z"){
-                    return 3 + 
+                        return 3 + adder
                 }
                 
                 if (line[0] == "A" && line[1] == "Z") ||
                     (line[0] == "B" && line[1] == "X") ||
                     (line[0] == "C" && line[1] == "Y"){
-                    return 0
+                    return 0 + adder
                 }
                 
                 if (line[0] == "A" && line[1] == "Y") ||
                     (line[0] == "B" && line[1] == "Z") ||
                     (line[0] == "C" && line[1] == "X"){
-                    return 6
+                    return 6 + adder
                 }
 
 
