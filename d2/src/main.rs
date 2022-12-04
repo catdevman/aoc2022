26318a81900967b7ee4654d11e70070f940d84e3
Lucas Pearson <catdevman@gmail.com>
2022-12-04 18:37:24 -0500

    comment out part 1

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -2,39 +2,40 @@ fn main() {
     // A, X - Rock
     // B, Y - Paper
     // C, Z - Scissor
-    let sum: i32 = include_str!("input")
-        .split("\n")
-        .map(|x|{
-            let line = x.split(" ").collect::<Vec<&str>>();            
-            if line.len() == 2{
-                let adder = match line[1] {
-		            "X" => 1,
-		            "Y" => 2,
-                    "Z" => 3,
-                    &_ => 0
-                };
-                if (line[0] == "A" && line[1] == "X") ||
-                    (line[0] == "B" && line[1] == "Y") ||
-                    (line[0] == "C" && line[1] == "Z"){
-                        return 3 + adder
-                }
-                
-                if (line[0] == "A" && line[1] == "Z") ||
-                    (line[0] == "B" && line[1] == "X") ||
-                    (line[0] == "C" && line[1] == "Y"){
-                    return 0 + adder
-                }
-                
-                if (line[0] == "A" && line[1] == "Y") ||
-                    (line[0] == "B" && line[1] == "Z") ||
-                    (line[0] == "C" && line[1] == "X"){
-                    return 6 + adder
-                }
+    // Part 1
+    //let sum: i32 = include_str!("input")
+    //    .split("\n")
+    //    .map(|x|{
+    //        let line = x.split(" ").collect::<Vec<&str>>();            
+    //        if line.len() == 2{
+    //            let adder = match line[1] {
+	//	            "X" => 1,
+	//	            "Y" => 2,
+    //                "Z" => 3,
+    //                &_ => 0
+    //            };
+    //            if (line[0] == "A" && line[1] == "X") ||
+    //                (line[0] == "B" && line[1] == "Y") ||
+    //                (line[0] == "C" && line[1] == "Z"){
+    //                    return 3 + adder
+    //            }
+    //            
+    //            if (line[0] == "A" && line[1] == "Z") ||
+    //                (line[0] == "B" && line[1] == "X") ||
+    //                (line[0] == "C" && line[1] == "Y"){
+    //                return 0 + adder
+    //            }
+    //            
+    //            if (line[0] == "A" && line[1] == "Y") ||
+    //                (line[0] == "B" && line[1] == "Z") ||
+    //                (line[0] == "C" && line[1] == "X"){
+    //                return 6 + adder
+    //            }
 
 
-            }
-            return 0;
-        })
-        .collect::<Vec<i32>>().iter().sum();
+    //        }
+    //        return 0;
+    //    })
+    //    .collect::<Vec<i32>>().iter().sum();
     println!("{:?}", sum)
 }
